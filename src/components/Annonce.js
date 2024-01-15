859e7ee928c27e4107b30198f5f753e6ed534951
Judi <judiherinirina04@gmail.com>
2024-01-15 14:26:26 +0300

    add other button annonce

--- src/components/Annonce.js
+++ src/components/Annonce.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../assets/css/Annonce.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Modal,Button } from 'react-bootstrap';
+import { faInfo, faShoppingCart, faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Annonce = ({ titre, description, date, vendeur, marqueVoiture, type, prix, commissionPropose, onDetailClick }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [action, setAction] = useState('');
+
+  const handleActionClick = (action) => {
+    setShowModal(true);
+    setAction(action);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setAction('');
+  };
+
+  const handleConfirm = () => {
+    // Ajoutez ici la logique pour gérer le clic sur "Oui"
+    // Vous pouvez utiliser l'état `action` pour déterminer quelle action a été confirmée
+    console.log(`Action confirmée: ${action}`);
+    handleClose();
+  };
   return (
     <div className="animated-annonce-card">
       <div className="card">
@@ -17,11 +40,40 @@ const Annonce = ({ titre, description, date, vendeur, marqueVoiture, type, prix,
             <li className="list-group-item"><strong>Prix:</strong> ${prix}</li>
             <li className="list-group-item"><strong>Commission proposée:</strong> ${commissionPropose}</li>
           </ul>
+          <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div class="btn-group me-2" role="group" aria-label="First group">
           <button className="btn btn-primary mt-3" onClick={onDetailClick}>
-            Voir le détail
+            <FontAwesomeIcon icon={faInfo} /> Detail
           </button>
+          <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Acheter')}>
+            <FontAwesomeIcon icon={faShoppingCart} /> Acheter
+          </button>
+          <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Contacter')}>
+            <FontAwesomeIcon icon={faEnvelope} /> Contacter
+          </button>
+          <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Favoris')}>
+            <FontAwesomeIcon icon={faHeart} /> Favoris
+          </button>
+          </div>
+          </div>
         </div>
       </div>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Êtes-vous sûr de vouloir {action.toLowerCase()} ?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Non
+          </Button>
+          <Button variant="primary" onClick={handleConfirm}>
+            Oui
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- src/components/AnnonceDetail.js
+++ src/components/AnnonceDetail.js
@@ -3,8 +3,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../assets/css/AnnonceDetail.css';
+import { faEnvelope, faHeart,  faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { Button,Modal } from 'react-bootstrap';
 
 const AnnonceDetail = ({ annonce, onFermerClick }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [action, setAction] = useState('');
+
+  const handleActionClick = (action) => {
+    setShowModal(true);
+    setAction(action);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setAction('');
+  };
+
+  const handleConfirm = () => {
+    // Ajoutez ici la logique pour gérer le clic sur "Oui"
+    // Vous pouvez utiliser l'état `action` pour déterminer quelle action a été confirmée
+    console.log(`Action confirmée: ${action}`);
+    handleClose();
+  };
   if (!annonce) {
     return null;
   }
@@ -24,11 +47,40 @@ const AnnonceDetail = ({ annonce, onFermerClick }) => {
             <li className="list-group-item"><strong>Prix:</strong> ${annonce.prix}</li>
             <li className="list-group-item"><strong>Commission proposée:</strong> ${annonce.commissionPropose}</li>
           </ul>
-          <button className="btn btn-ferme mt-3" onClick={onFermerClick}>
-            Fermer
-          </button>
+          <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div class="btn-group me-2" role="group" aria-label="First group">
+            <button className="btn btn-ferme btn-primary mt-3" onClick={onFermerClick} >
+               Ferme
+            </button>
+            <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Acheter')}>
+              <FontAwesomeIcon icon={faShoppingCart} /> Acheter
+            </button>
+            <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Contacter')}>
+              <FontAwesomeIcon icon={faEnvelope} /> Contacter
+            </button>
+            <button className="btn btn-primary mt-3" onClick={() => handleActionClick('Favoris')}>
+              <FontAwesomeIcon icon={faHeart} /> Favoris
+            </button>
+          </div>
+          </div>
         </div>
       </div>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Êtes-vous sûr de vouloir {action.toLowerCase()} ?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Non
+          </Button>
+          <Button variant="primary" onClick={handleConfirm}>
+            Oui
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
