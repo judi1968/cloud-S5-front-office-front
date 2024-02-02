8dab67712f456c8f4b28592e40ce1d67256941e5
Judi <judiherinirina04@gmail.com>
2024-02-02 09:36:02 +0300

    create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
 import './../assets component/css/Footer.css'
+import { FaFacebook, FaGithubAlt, FaPhoneAlt } from 'react-icons/fa';
+import { AiFillMail } from 'react-icons/ai';
+
 const Footer = () => {
     return(
         <div className="row footer">
-            footer
+            <div className='col-md-6 propos-part'>
+                jblkqr
+            </div>
+            <div className='col-md-6 contact-part'>
+                <h1>Contact</h1>
+                <ul>
+                    <li>ETU001968</li>
+                    <li><FaPhoneAlt className='contact-icon'></FaPhoneAlt>0340460137</li>
+                    <li><AiFillMail className='contact-icon'></AiFillMail>judiherinirina@gmail.com</li>
+                    <li><FaFacebook className='contact-icon'></FaFacebook>www.facebook/judi.nh.04</li>
+                    <li><FaGithubAlt className='contact-icon'></FaGithubAlt>https://github.com/judi1968</li>
+                </ul>
+            </div>
         </div>
     );
 }
