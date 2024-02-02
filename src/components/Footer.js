928b040dba27aebc63a81d31f2da9d3c99ebe34d
Judi <judiherinirina04@gmail.com>
2024-02-02 10:30:59 +0300

    finish header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import './../assets component/css/Footer.css'
 import { FaFacebook, FaGithubAlt, FaPhoneAlt } from 'react-icons/fa';
 import { AiFillMail } from 'react-icons/ai';
+import { PiStudentFill } from 'react-icons/pi'
 
 const Footer = () => {
     return(
@@ -11,10 +12,10 @@ const Footer = () => {
             <div className='col-md-6 contact-part'>
                 <h1>Contact</h1>
                 <ul>
-                    <li>ETU001968</li>
+                    <li><PiStudentFill className='contact-icon' />ETU001968</li>
                     <li><FaPhoneAlt className='contact-icon'></FaPhoneAlt>0340460137</li>
                     <li><AiFillMail className='contact-icon'></AiFillMail>judiherinirina@gmail.com</li>
-                    <li><FaFacebook className='contact-icon'></FaFacebook>www.facebook/judi.nh.04</li>
+                    <li><FaFacebook className='contact-icon'></FaFacebook>web.facebook.com/judi.nh.04/</li>
                     <li><FaGithubAlt className='contact-icon'></FaGithubAlt>https://github.com/judi1968</li>
                 </ul>
             </div>
