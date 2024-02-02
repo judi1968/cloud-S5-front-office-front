86e080424b991ab33d1e8154f5363306fc0226ea
Judi <judiherinirina04@gmail.com>
2024-02-02 11:49:38 +0300

    finish carousel

--- src/components/ControlledCarousel.js
+++ src/components/ControlledCarousel.js
@@ -1,43 +1,42 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function ControlledCarousel() {
   return (
-    <Carousel data-bs-theme="dark">
+    <Carousel data-bs-theme="light">
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="%PUBLIC_URL%/../assets/images/logo/logo_trial.png"
+          src="%PUBLIC_URL%/../assets/images/carousel_accueil/carousel img1.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h5>Vente de voiture</h5>
+          <p>Vous pouvez vendre des voitures ici</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="%PUBLIC_URL%/../assets/images/logo/logo_trial.png"
+          src="%PUBLIC_URL%/../assets/images/carousel_accueil/carousel img2.jpg"
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h5>Voiture d'occasion</h5>
+          <p>Meme si c'est un voiture d'occasion , vous pouvez le vendre ici</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="%PUBLIC_URL%/../assets/images/logo/logo_trial.png"
+          src="%PUBLIC_URL%/../assets/images/carousel_accueil/carousel img3.jpg"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h5>Tout les voitures</h5>
+          <p>Vous pouvez vendre tout les voitures que vous voulez</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
