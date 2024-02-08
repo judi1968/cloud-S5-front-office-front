09c845b3a6b3dcd104ef478b5afee6967383117e
Judi <judiherinirina04@gmail.com>
2024-02-08 23:56:07 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,45 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import './../assets component/css/Header.css';
 import { FaNewspaper, FaPowerOff, FaRegUserCircle } from 'react-icons/fa';
-import {AiOutlineMessage } from 'react-icons/ai';
-import { GoHome } from 'react-icons/go'
+import { AiOutlineMessage } from 'react-icons/ai';
+import { GoHome } from 'react-icons/go';
 import { useEffect, useState } from 'react';
 import { connect_token } from '../services/token.service';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const [etatConnection,setEtatConnection] = useState([])
-  const [isConnected,setIsConected] = useState()
-  const [username,setUsername] = useState('')
+  const [isConnected, setIsConnected] = useState(false);
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+
   const checkConnection = async () => {
     try {
-        const data = await connect_token();
-
-        const personne = data;
-        setEtatConnection(personne)
-        if (etatConnection.status===200) {
-          setIsConected(true)
-          setUsername(personne.personne.personne.nom)
-        }else{
-          setIsConected(false)
-        }
-
+      const data = await connect_token();
+      if (data.status === 200) {
+        setIsConnected(true);
+        setUsername(data.personne.personne.nom);
+      } else {
+        setIsConnected(false);
+      }
     } catch (error) {
-        console.error('Une erreur est survenue lors de la récupération de la personne:', error);
+      console.error('Une erreur est survenue lors de la récupération de la personne:', error);
     }
-};
-useEffect(() => {
-  checkConnection();
-}, []);
-const navigate = useNavigate();
-const disconnectAction = () => {
-  localStorage.removeItem();
-  navigate('/')
-}
+  };
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+  const disconnectAction = () => {
+    localStorage.removeItem('tknidclient');
+    setIsConnected(false);
+    setUsername('');
+    navigate('/');
+  };
+
   return (
     <div className='header'>
       {['md'].map((expand) => (
@@ -58,29 +58,19 @@ const disconnectAction = () => {
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body className=''>
-            
                 <Nav className="justify-content-end ">
-                  <Nav.Link href="/" className='d-flex items-center header-link'><GoHome className='icon-header'/><span>Accueil</span></Nav.Link>
-                  <Nav.Link href="/list-annonce" className='d-flex items-center header-link'><FaNewspaper className='icon-header'/><span>Annonce</span></Nav.Link>
-                  <Nav.Link href="/message" className='d-flex items-center header-link'><AiOutlineMessage className='icon-header'/><span>Message</span></Nav.Link>
-                  {isConnected?(
-                    <>
-                    <Nav.Link href="#action1" className='d-flex items-center header-link' style={{color:'black'}}><FaRegUserCircle className='icon-header'/><span>{username}</span></Nav.Link>
-                  </>
-                 ) : (
-                 <>
-                 </>
-                  )}
+                  <Nav.Link href="/" className='d-flex items-center header-link'><GoHome className='icon-header' /><span>Accueil</span></Nav.Link>
+                  <Nav.Link href="/list-annonce" className='d-flex items-center header-link'><FaNewspaper className='icon-header' /><span>Annonce</span></Nav.Link>
+                  <Nav.Link href="/message" className='d-flex items-center header-link'><AiOutlineMessage className='icon-header' /><span>Message</span></Nav.Link>
+                  {isConnected ? (
+                    <Nav.Link href="#action1" className='d-flex items-center header-link' style={{ color: 'black' }}><FaRegUserCircle className='icon-header' /><span>{username}</span></Nav.Link>
+                  ) : null}
                 </Nav>
-                {!isConnected?(
-                  <>
-                    <a className='btn btn-success d-flex items-center btn-deconnection' href='/login'><FaPowerOff className='icon-header'></FaPowerOff><span> Connecter</span></a>
-                  </>
-                 ) : (
-                 <>
-                   <a className='btn btn-danger d-flex items-center btn-deconnection' onClick={disconnectAction}><FaPowerOff className='icon-header'></FaPowerOff><span> deconnecter</span></a>
-                 </>
-                  )}
+                {!isConnected ? (
+                  <a className='btn btn-success d-flex items-center btn-deconnection' href='/login'><FaPowerOff className='icon-header'></FaPowerOff><span> Connecter</span></a>
+                ) : (
+                  <a className='btn btn-danger d-flex items-center btn-deconnection' onClick={disconnectAction}><FaPowerOff className='icon-header'></FaPowerOff><span> deconnecter</span></a>
+                )}
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
