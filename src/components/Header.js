712d57d5044e3ccdba83830283d9dfe9d9ebae66
Judi <judiherinirina04@gmail.com>
2024-02-08 23:57:31 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
